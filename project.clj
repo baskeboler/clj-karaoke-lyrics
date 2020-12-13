38edae4b0e650c7b44796671fde41576373c1c87
Victor Gil <baskeboler@gmail.com>
2020-12-13 18:55:33 -0300

    updated project.clj for clojars deployment

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-karaoke-lyrics "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject baskeboler/clj-karaoke-lyrics "1.0.0-SNAPSHOT"
+  :description "Rip karaoke lyrics from karaoke midi files"
   :url "http://github.com/baskeboler/clj-karaoke-lyrics.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -10,4 +10,12 @@
                  [clj-cli-progress "0.1.0"]]
   :main ^:skip-aot clj-karaoke.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all #_[clj-karaoke.core clj-karaoke.karaoke clj-karaoke.lyrics]}})
+  :profiles {:uberjar {:aot :all #_[clj-karaoke.core clj-karaoke.karaoke clj-karaoke.lyrics]}}
+  :repositories
+  {"clojars" {:url "https://clojars.org/repo"
+              :sign-releases false
+              :username "baskeboler"
+              :password :env/CLOJARS_TOKEN}
+   "github" {:url "https://maven.pkg.github.com/baskeboler/cljs-karaoke-lyrics"
+             :sign-releases false}}
+  :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]])
